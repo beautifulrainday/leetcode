e409d1e2b5474931fc39d43d1e617ba8c871d224
beautifulrainday <854018001@qq.com>
2021-04-03 19:51:22 +0800

    simple  反转链表！！ 记住前值，当前的后值为前值  这是思路

---
+++ src/leetcode/simple/ReverseList.java
@@ -0,0 +1,59 @@
+package leetcode.simple;
+
+//反转一个单链表。
+//
+// 示例:
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL
+//
+// 进阶:
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+// Related Topics 链表
+// 👍 1651 👎 0
+public class ReverseList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode preNode = null;//上一个值
+        ListNode curNode = head;//当前值
+        //要做的是让上面成为当前值的下面
+        while (curNode != null) {
+            ListNode tempNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = tempNode;
+        }
+        return preNode;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = null;
+        ListNode resNode = reverseList(node1);
+        while (resNode != null) {
+            System.out.println(resNode.val);
+            resNode = resNode.next;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
